00147b1bf7ea59514ee3133094b81185c9e2d71e
Steven Rakhmanchik <48323187+stevenrakhmanchik@users.noreply.github.com>
2022-06-08 22:07:53 -0400

    switched wget to curl to work without brew

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "–––––––––––––––––––––––––––––––––––––––––––––––––––"
 echo "Starting Installation"
 echo "–––––––––––––––––––––––––––––––––––––––––––––––––––"
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 . $HOME/miniconda/bin/activate
 yes | conda create --name icbm python=2.7
